be7996589f6fbc59bf78db68bf2c20a97c68c887
Mevek <148761491+Mevek@users.noreply.github.com>
2024-06-07 10:52:28 +0200

    v1

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+    <p className=" text-violet-600 text-5xl font-bold">Jakub Karnicki</p>
+    <p className="text-xl">Nr albumu: 14216</p>
+    </div>
+  );
+}
